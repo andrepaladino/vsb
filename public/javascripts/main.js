452a706196275db358689c212ab6f29c267ef77c
André Paladino <akp.paladino@gmail.com>
2020-05-08 16:36:33 -0300

    filter status with icons

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -64,7 +64,7 @@ $(document).ready(function () {
             $("#myTable tr").show()
         } else {
             $("#myTable tr").filter(function () {
-                $(this).toggle($(this).children(':eq(3)').text().indexOf(status) > -1)
+                $(this).toggle($(this).children(':eq(3)').find('input[name=actionStatus]').val().indexOf(status) > -1)
             });
         }
     })
